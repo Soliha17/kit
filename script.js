338d87fc75b13a427d0195d0019fc5c9aeef6ba0
Soliha <solyatursuntasheva@gmail.com>
2023-01-21 12:27:30 +0500

    upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var telInput = $(".phone"),
+  errorMsg = $(".error-msg"),
+  validMsg = $(".valid-msg");
+
+// initialise plugin
+telInput.intlTelInput({
+  allowExtensions: true,
+  formatOnDisplay: true,
+  autoFormat: true,
+  autoHideDialCode: true,
+  autoPlaceholder: true,
+  defaultCountry: "auto",
+  ipinfoToken: "yolo",
+
+  nationalMode: false,
+  numberType: "MOBILE",
+  //onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+  preferredCountries: ["sa", "ae", "qa", "om", "bh", "kw", "ma"],
+  preventInvalidNumbers: true,
+  separateDialCode: true,
+  initialCountry: "ua",
+  geoIpLookup: function (callback) {
+    $.get("http://ipinfo.io", function () {}, "jsonp").always(function (resp) {
+      var countryCode = resp && resp.country ? resp.country : "";
+      callback(countryCode);
+    });
+  },
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.9/js/utils.js",
+});
+
+var reset = function () {
+  telInput.removeClass("error");
+  errorMsg.addClass("hide");
+  validMsg.addClass("hide");
+};
+
+// on blur: validate
+telInput.blur(function () {
+  reset();
+  if ($.trim(telInput.val())) {
+    if (telInput.intlTelInput("isValidNumber")) {
+      validMsg.removeClass("hide");
+    } else {
+      telInput.addClass("error");
+      errorMsg.removeClass("hide");
+    }
+  }
+});
+
+// on keyup / change flag: reset
+telInput.on("keyup change", reset);
+// menu
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
